8db38e7708fcba19b173f613f6c61a29e98c4d06
Anuj Pandey <anujpandey2024@gmail.com>
2026-08-29 23:29:10 +0530

    Fix two-layer loss call and initialization

--- src/mydl/models/two_layer_nn.py
+++ src/mydl/models/two_layer_nn.py
@@ -1,20 +1,20 @@
-import numpy as np 
-from ..initializers import zeros,random
+import numpy as np
+from ..initializers import zeros, random
 from ..layers import linear_activation_forward, linear_activation_backward
 from ..activations import relu, sigmoid, sigmoid_backward, relu_backward
 from ..losses import binary_cross_entropy, binary_cross_entropy_backward
 from ..metrics import accuracy
 
+
 class TwoLayerNN:
 
     def __init__(
         self,
-        hidden_units: int=16,
-        learning_rate: float=0.01,
-        epochs: int=100,
-        threshold: float=0.5,
-        verbose = True,
-        
+        hidden_units: int = 16,
+        learning_rate: float = 0.01,
+        epochs: int = 100,
+        threshold: float = 0.5,
+        verbose=True,
     ):
         self.hidden_units = hidden_units
         self.learning_rate = learning_rate
@@ -30,7 +30,7 @@ class TwoLayerNN:
         self.is_fitted = False
         self.verbose = verbose
 
-    def _initialize_parameters(self,n_features):
+    def _initialize_parameters(self, n_features):
         self.W1 = random(
             (self.hidden_units, n_features),
             np.sqrt(2 / n_features),
@@ -38,53 +38,65 @@ class TwoLayerNN:
         self.b1 = zeros((self.hidden_units, 1))
         self.W2 = random(
             (1, self.hidden_units),
-            np.sqrt(2 / n_features),
+            np.sqrt(2 / self.hidden_units),
         )
-        self.b2 = zeros((1,1))
+        self.b2 = zeros((1, 1))
 
     def _forward(self, X: np.ndarray):
         A1, cache1 = linear_activation_forward(X, self.W1, self.b1, relu)
         A2, cache2 = linear_activation_forward(A1, self.W2, self.b2, sigmoid)
 
-        caches = (cache1,cache2)
+        caches = (cache1, cache2)
         self.A2 = A2
 
         return A2, caches
 
-    def _compute_loss(self, Y, A2 ):
+    def _compute_loss(self, Y, A2):
         return binary_cross_entropy(Y, A2)
 
-    def _backward(self, Y, A2,caches):
+    def _backward(self, Y, A2, caches):
         dA2 = binary_cross_entropy_backward(Y, A2)
-        dA1, dW2, db2 = linear_activation_backward(dA2, caches[1], sigmoid_backward)
-        _, dW1, db1 = linear_activation_backward(dA1, caches[0], relu_backward)
+        dA1, dW2, db2 = linear_activation_backward(
+            dA2,
+            caches[1],
+            sigmoid_backward,
+        )
+        _, dW1, db1 = linear_activation_backward(
+            dA1,
+            caches[0],
+            relu_backward,
+        )
 
         self.dW1 = dW1
         self.db1 = db1
         self.dW2 = dW2
         self.db2 = db2
 
-    def _update_parameters(self,):
+    def _update_parameters(self):
         self.W1 = self.W1 - self.learning_rate * self.dW1
         self.b1 = self.b1 - self.learning_rate * self.db1
         self.W2 = self.W2 - self.learning_rate * self.dW2
         self.b2 = self.b2 - self.learning_rate * self.db2
 
-    def fit(self,X_test: np.ndarray, Y_test: np.ndarray):
-        n_features,m = X_test.shape
+    def fit(self, X: np.ndarray, Y: np.ndarray):
+        n_features, m = X.shape
 
         self._initialize_parameters(n_features)
 
         for epoch in range(self.epochs):
-            # forward propogation
-            prediction, caches = self._forward(X_test)
-            # compute loss
-            cost = self._compute_loss(Y_test, prediction)
-            # backward propagation
-            self._backward(Y_test, prediction, caches)
+            # Forward propagation
+            prediction, caches = self._forward(X)
+
+            # Compute loss
+            cost = self._compute_loss(Y, prediction)
+
+            # Backward propagation
+            self._backward(Y, prediction, caches)
+
             # Gradient descent
             self._update_parameters()
-            # save cost
+
+            # Save cost
             self.cost_history.append(cost)
 
             if self.verbose and epoch % 100 == 0:
@@ -95,15 +107,14 @@ class TwoLayerNN:
         return self
 
     def predict_proba(self, X):
-        A2,_ = self._forward(X)
+        if not self.is_fitted:
+            raise ValueError("Model has not been fitted yet.")
+
+        A2, _ = self._forward(X)
 
         return A2
 
     def predict(self, X):
-
-        if not self.is_fitted:
-            raise ValueError("Model has not been fitted yet.")
-
         probability = self.predict_proba(X)
         predictions = (probability >= self.threshold).astype(int)
 
@@ -113,4 +124,4 @@ class TwoLayerNN:
         predictions = self.predict(X)
         score = accuracy(Y, predictions)
 
-        return score 
+        return score
