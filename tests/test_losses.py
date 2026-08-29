c7af8ec66bfa7fbc4cf8cc25e3f3b85213be8582
Anuj Pandey <anujpandey2024@gmail.com>
2026-08-29 23:35:10 +0530

    Align loss gradient tests with mean-loss convention

--- tests/test_losses.py
+++ tests/test_losses.py
@@ -17,7 +17,6 @@ def test_binary_cross_entropy():
     y_pred = np.array([[0.9, 0.2, 0.8]])
 
     loss = binary_cross_entropy(y_true, y_pred)
-
     expected = -np.mean(
         y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred)
     )
@@ -31,32 +30,20 @@ def test_binary_cross_entropy_backward():
     y_pred = np.array([[0.9, 0.2, 0.8]])
 
     grad = binary_cross_entropy_backward(y_true, y_pred)
-
-    expected = -(y_true / y_pred - (1 - y_true) / (1 - y_pred))
+    m = y_true.shape[1]
+    expected = -(1 / m) * (
+        y_true / y_pred - (1 - y_true) / (1 - y_pred)
+    )
 
     assert grad.shape == y_pred.shape
     assert np.allclose(grad, expected)
 
 
 def test_categorical_cross_entropy():
-    y_true = np.array(
-        [
-            [1, 0],
-            [0, 1],
-            [0, 0],
-        ]
-    )
-
-    y_pred = np.array(
-        [
-            [0.8, 0.1],
-            [0.1, 0.8],
-            [0.1, 0.1],
-        ]
-    )
+    y_true = np.array([[1, 0], [0, 1], [0, 0]])
+    y_pred = np.array([[0.8, 0.1], [0.1, 0.8], [0.1, 0.1]])
 
     loss = categorical_cross_entropy(y_true, y_pred)
-
     expected = -(1 / 2) * np.sum(y_true * np.log(y_pred))
 
     assert isinstance(loss, float)
@@ -64,21 +51,8 @@ def test_categorical_cross_entropy():
 
 
 def test_categorical_cross_entropy_backward():
-    y_true = np.array(
-        [
-            [1, 0],
-            [0, 1],
-            [0, 0],
-        ]
-    )
-
-    y_pred = np.array(
-        [
-            [0.8, 0.1],
-            [0.1, 0.8],
-            [0.1, 0.1],
-        ]
-    )
+    y_true = np.array([[1, 0], [0, 1], [0, 0]])
+    y_pred = np.array([[0.8, 0.1], [0.1, 0.8], [0.1, 0.1]])
 
     grad = categorical_cross_entropy_backward(y_true, y_pred)
     expected = -(y_true / y_pred) / y_true.shape[1]
@@ -92,7 +66,6 @@ def test_mean_squared_error():
     y_pred = np.array([[1.1, 1.9, 3.2]])
 
     loss = mean_squared_error(y_true, y_pred)
-
     expected = np.mean((y_pred - y_true) ** 2)
 
     assert isinstance(loss, float)
@@ -115,7 +88,6 @@ def test_mean_absolute_error():
     y_pred = np.array([[1.1, 1.9, 3.2]])
 
     loss = mean_absolute_error(y_true, y_pred)
-
     expected = np.mean(np.abs(y_pred - y_true))
 
     assert isinstance(loss, float)
