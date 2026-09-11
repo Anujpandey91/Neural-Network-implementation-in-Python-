e4b69c5dac74fe38ded90bc634d3de48c10c5c24
anuj pandey <anujpandey@anup.local>
2026-09-11 23:58:12 +0530

    Added GradientDescent optimzers

---
+++ src/mydl/optimizers/gradient_descent.py
@@ -0,0 +1,16 @@
+class GradientDescent:
+
+    def __init__(self, learning_rate):
+        self.learning_rate = learning_rate
+
+    def update(self, parameters, grads):
+
+        L = len(parameters) // 2
+
+        for l in range(1, L + 1):
+            parameters["W" + str(l)] = (
+                parameters["W" + str(l)] - self.learning_rate * grads["dW" + str(l)]
+            )
+            parameters["b" + str(l)] = (
+                parameters["b" + str(l)] - self.learning_rate * grads["db" + str(l)]
+            )
